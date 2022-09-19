7cc43bc91eb62aa030ad942583f109d2137f70d9
matt2930 <matthewa@rogers.com>
2022-09-19 13:04:16 -0400

    feat: add args for data input

--- tmpl8/cli.py
+++ tmpl8/cli.py
@@ -1,25 +1,45 @@
 import argparse
+import json
 import os
+import yaml
 
 from tempfile import TemporaryDirectory
 from tmpl8 import __version__
 from tmpl8.command import Command
+from tmpl8.data import validateJsonData, validateFile
 from tmpl8.template import templateFile
 
 def main():
     parser = argparse.ArgumentParser(prog='tmpl8', description="tmpl8 wrapper for cli commands")
-    parser.add_argument('command', help='command to run', nargs=argparse.REMAINDER)
     parser.add_argument('-v', '--version', action='version', version=__version__)
+    command_group = parser.add_argument_group('command')
+    command_group.add_argument('command', help='command to run', nargs=1, default=[])
+    command_group.add_argument('args', nargs=argparse.REMAINDER, help=argparse.SUPPRESS)
+
+    data_group = parser.add_argument_group('data', 'data used when templating')
+    data_group.add_argument('-d', '--data', help='json data for templating. Overwrites data from file if -f/--file is specified', type=validateJsonData)
+    data_group.add_argument('-f', '--file', help='file with data to use for templating', type=validateFile)
 
     args = parser.parse_args()
 
+    error_message = 'error:'
+
     if args.command == []:
+        error_message = f'{error_message} missing command'
+
+    if not args.data and not args.file:
+        error_message = f'{error_message} missing data'
+
+    if error_message != 'error:':
         parser.print_help()
-        parser.exit(status=1, message='error: missing command\n')
+        parser.exit(status=1, message=f'{error_message}\n')
 
-    command = Command(args.command)
+    command = Command(args.command + args.args)
 
-    data = {}
+    args.data = args.data or {}
+    args.file = args.file or {}
+
+    data = {**args.file, **args.data}
 
     with TemporaryDirectory() as td:
         for arg_info in command.arg_info:
---
+++ tmpl8/data.py
@@ -0,0 +1,24 @@
+import argparse
+import json
+import os
+import yaml
+
+def validateJsonData(value):
+    data = json.loads(value)
+    return data
+
+def validateYamlData(value):
+    data = yaml.safe_load(value)
+    return data
+
+def validateFile(value):
+    try:
+        with open(value, 'r') as f:
+            data = ''.join(f.readlines())
+    except FileNotFoundError:
+        raise argparse.ArgumentTypeError(f'no file: {value}')
+
+    if os.path.splitext(value)[1] in ('.yml', '.yaml'):
+        return validateYamlData(data)
+
+    return validateJsonData(data)
